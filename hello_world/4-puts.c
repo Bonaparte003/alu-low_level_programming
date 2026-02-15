af6717a3472a95e989962a6cce25d54ef8d235d5
Avellin003 <uavellinbonaparte@gmail.com>
2026-02-15 15:59:43 +0200

    puts function that prints out a string

---
+++ hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * A function that outputs
+ * "Programming is like building a multilingual puzzle"
+ */
+
+void main(void){
+    puts("\"Programming is like building a multilingual puzzle\n");
+}
