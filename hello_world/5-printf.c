987214c97d67b41d287d7ab38a3e163cd192b9c5
Avellin003 <uavellinbonaparte@gmail.com>
2026-02-15 16:09:52 +0200

    betty compliant

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
- * A function that uses print
+ *main - starting point for program execution
+ *printf - send formatted output to the screen
+ *Return: Always 0 (Success)
  */
-
-int main(){
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return 0;
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
