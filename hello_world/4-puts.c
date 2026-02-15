58382f1b30a8f22d35c0d01063eaa8f925522413
Avellin003 <uavellinbonaparte@gmail.com>
2026-02-15 16:04:46 +0200

    added the return of number 0 to show that compilation went through

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,16 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
- * A function that outputs
- * "Programming is like building a multilingual puzzle"
+ *  main - starting point for program execution
+ * description - main will be used as function
+ *  Return: 0 (success)
  */
 
-int main(){
-    puts("\"Programming is like building a multilingual puzzle");
-    puts("\n");
-    return 0;
+
+int main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
 }
